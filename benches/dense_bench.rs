2740228881f1fb0c2c0bfe56ea2162ced23364d3
Shidowy <101477459+Shidowy@users.noreply.github.com>
2025-02-04 16:57:14 -0800

    add: dense_benches

--- benches/dense_bench.rs
+++ benches/dense_bench.rs
@@ -1,1 +1,16 @@
+use criterion::{Criterion, criterion_group, criterion_main};
 
+use linear_algebra_ml::dense::matrix::Matrix;
+use linear_algebra_ml::dense::ops;
+
+fn bench_mul(c: &mut Criterion) {
+    let a = Matrix::new(100, 100, vec![1.0; 100 * 100]); 
+    let b = Matrix::new(100, 100, vec![2.0; 100 * 100]);
+
+    c.bench_function("matrix multiplication 100x100", |bencher| {
+        bencher.iter(|| ops::mul(&a, &b).unwrap())
+    });
+}
+
+criterion_group!(benches, bench_mul);
+criterion_main!(benches);
