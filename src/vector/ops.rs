bed45582da8c642afa7229dfde3a0b09b407a561
Shidowy <101477459+Shidowy@users.noreply.github.com>
2025-02-07 11:45:19 -0800

    added all basic vector ops

--- src/vector/ops.rs
+++ src/vector/ops.rs
@@ -11,3 +11,47 @@ pub fn add(a: &Vector, b: &Vector) -> Result<Vector, Error> {
     }
     Ok(result)
 }
+
+pub fn sub(a: &Vector, b: &Vector) -> Result<Vector, Error> {
+    if a.len() != b.len() {
+        return Err(Error::MatrixSizeMismatch);
+    }
+    let mut result = Vector::new(vec![0.0; a.len()]);
+    for i in 0..a.len() {
+        result.set(i, a.get(i) - b.get(i));
+    }
+    Ok(result)
+}
+
+pub fn mul(a: &Vector, b: &Vector) -> Result<Vector, Error> {
+    if a.len() != b.len() {
+        return Err(Error::MatrixSizeMismatch);
+    }
+    let mut result = Vector::new(vec![0.0; a.len()]);
+    for i in 0..a.len() {
+        result.set(i, a.get(i) * b.get(i));
+    }
+    Ok(result)
+}
+
+pub fn div(a: &Vector, b: &Vector) -> Result<Vector, Error> {
+    if a.len() != b.len() {
+        return Err(Error::MatrixSizeMismatch);
+    }
+    let mut result = Vector::new(vec![0.0; a.len()]);
+    for i in 0..a.len() {
+        result.set(i, a.get(i) / b.get(i));
+    }
+    Ok(result)
+}
+
+pub fn dot(a: &Vector, b: &Vector) -> Result<f64, Error> {
+    if a.len() != b.len() {
+        return Err(Error::MatrixSizeMismatch);
+    }
+    let mut result = 0.0;
+    for i in 0..a.len() {
+        result += a.get(i) * b.get(i);
+    }
+    Ok(result)
+}
--- tests/vector_tests.rs
+++ tests/vector_tests.rs
@@ -9,3 +9,35 @@ fn test_vector_add() {
     let result = ops::add(&a, &b).unwrap();
     assert_eq!(result, Vector::new(vec![5.0, 7.0, 9.0]));
 }
+
+#[test]
+fn test_vector_sub() {
+    let a = Vector::new(vec![1.0, 2.0, 3.0]);
+    let b = Vector::new(vec![4.0, 5.0, 6.0]);
+    let result = ops::sub(&a, &b).unwrap();
+    assert_eq!(result, Vector::new(vec![-3.0, -3.0, -3.0]));
+}
+
+#[test]
+fn test_vector_mul() {
+    let a = Vector::new(vec![1.0, 2.0, 3.0]);
+    let b = Vector::new(vec![4.0, 5.0, 6.0]);
+    let result = ops::mul(&a, &b).unwrap();
+    assert_eq!(result, Vector::new(vec![4.0, 10.0, 18.0]));
+}
+
+#[test]
+fn test_vector_div() {
+    let a = Vector::new(vec![1.0, 2.0, 3.0]);
+    let b = Vector::new(vec![4.0, 5.0, 6.0]);
+    let result = ops::div(&a, &b).unwrap();
+    assert_eq!(result, Vector::new(vec![0.25, 0.4, 0.5]));
+}
+
+#[test]
+fn test_vector_dot() {
+    let a = Vector::new(vec![1.0, 2.0, 3.0]);
+    let b = Vector::new(vec![4.0, 5.0, 6.0]);
+    let result = ops::dot(&a, &b).unwrap();
+    assert_eq!(result, 32.0);
+}
